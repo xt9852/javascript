7c8e0f5fe80e62ed631e8e8c94b576d8f88cc9b7
xt9852 <xt9852@github.com>
2023-07-12 22:24:30 +0800

    update js

--- tampermonkey_demo_2.js
+++ tampermonkey_demo_2.js
@@ -7,22 +7,25 @@
 // @match        https://v88avnetwork.github.io/88av.html*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @require      https://cdn.jsdelivr.net/npm/hls.js@latest
-// @connect      88av1143.cc
+// @connect      88av1424.cc
+// @connect      jubt.top
+// @connect      rm01.xyz
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_deleteValue
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-
-var dns;
-var url;
-var hls;
-var next;
-var last = null;
-
+var dns = '';
+var uri = '';
+var num = '';
+var hls = null;
+var video = null;
 
 function get_url() {
     console.log("--查找地址开始--");
+
+    //GM_deleteValue('domain');
 
     dns = GM_getValue('domain', '');
 
@@ -32,19 +35,17 @@ function get_url() {
         console.log('set dns:' + dns);
     }
 
-    url = dns;
-
     let ret;
-    let reg = /\?type=(.+?)&page=(\d*)/g;
+    let reg = /\?(.+)(\d+)$/g;
 
     if ((ret = reg.exec(window.location.search)) !== null) {
-        url += ret[1] + "/" + ret[2];
-        next = location.href.substring(0, location.href.lastIndexOf('=') + 1) + (parseInt(ret[2]) + 1);
+        uri = ret[1] + ret[2];
+        num = parseInt(ret[2]);
     }
 
     console.log('dns:' + dns);
-    console.log('url:' + url);
-    console.log('next:' + next);
+    console.log('uri:' + uri);
+    console.log('num:' + num);
 
     console.log("--查找地址结束--\n\n\n");
 }
@@ -66,36 +67,47 @@ function add_link() {
 
     document.body = document.createElement("body");
 
-    new_link('91', '?type=categories/91&page=1');
-    new_link('ca', '?type=search/ca&page=1');
-    new_link('jav', '?type=jav&page=1');
-    new_link('usa', '?type=oumei&page=1');
-    new_link('new', '?type=video/latest&page=1');
+    new_link('91', '?categories/91/1');
+    new_link('jav', '?jav/1');
+    new_link('usa', '?oumei/1');
+    new_link('search', '?search/ca/1');
 
     console.log("--添加连接结束--\n\n\n");
 }
 
 function get_m3u8() {
 
-    if (this == last) {
-        return;
-    }
-
-    if (last != null) {
-        last.controls = false;
-        last.style.cursor = "pointer";
+    if (video != null && video != this) {
         hls.destroy();
+        video.controls = false;
+        video.style.cursor = "pointer";
     }
 
-    last = this;
-    this.controls = true;
-    this.style.cursor = "default";
+    video = this;
+    video.controls = true;
+    video.style.cursor = "default";
 
-    hls = new Hls();
+    hls = new Hls({ id : this.id });
     hls.attachMedia(this);
-    hls.loadSource(dns + this.id);
+    hls.loadSource(dns + '/video/m3u8/' + this.id + '.m3u8?video_server=cncdn');
+
     hls.on(Hls.Events.MANIFEST_PARSED, function() {
-        this.play();
+        video.play();
+    });
+
+    hls.on(Hls.Events.ERROR, function (event, data) {
+        console.log('ERROR');
+
+        let url = 'https://7.bbdata.cc/videos/' + hls.config.id + '/g.m3u8';
+
+        hls.destroy();
+        hls = new Hls();
+        hls.attachMedia(video);
+        hls.loadSource(url);
+
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
+            video.play();
+        });
     });
 }
 
@@ -109,13 +121,12 @@ function add_page(html) {
     let reg = /<img alt="([^"]+)"[\s\S]+?\/\/([^\/]+)\/videos\/([^\/]+)\//g;
 
     for (var i = 1; (ret = reg.exec(html)) !== null; i++) {
-
         div = document.createElement("div");
         div.innerText = i + ': ' +ret[1];
         document.body.appendChild(div);
 
         img = document.createElement('video');
-        img.id = '/video/m3u8/' + ret[3] + '.m3u8?video_server=cncdn';
+        img.id = ret[3];
         img.poster = 'https://' + ret[2] + '/videos/' + ret[3] + '/cover/5_505_259';
         img.onclick = get_m3u8;
         img.style = 'cursor:pointer;max-width:100%;max-height:100%';
@@ -125,17 +136,12 @@ function add_page(html) {
     }
 
     reg = /data-total-page=\"([0-9]+)\"/;
-    if ((ret = reg.exec(html)) !== null) { txt = 'total-page=' + ret[1]; }
-
-    div = document.createElement("div");
-    div.innerText = txt;
-    document.body.appendChild(div);
-
-    console.log(txt);
+    if ((ret = reg.exec(html)) !== null) { txt = 'total=' + ret[1]; }
 
     div = document.createElement("a");
-    div.href = next;
-    div.innerText = 'next';
+    div.href = location.href.substring(0, location.href.lastIndexOf('/') + 1) + (num + 1);
+    div.innerText = txt;
+    div.style = 'float:right; bottom:0';
     document.body.appendChild(div);
 
     console.log("--处理页面结束--\n\n\n");
@@ -144,11 +150,11 @@ function add_page(html) {
 function get_data() {
     console.log("--请求页面开始--");
 
-    console.log(url);
+    console.log(dns + uri);
 
     GM_xmlhttpRequest({
       method: 'GET',
-      url: url,
+      url: dns + uri,
       onload(xhr) { add_page(xhr.responseText) }
     });
 
